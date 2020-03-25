6bdd707ea7fa4d107725c7c226aeaa83a1416da2
Travis CI <travis@travis-ci.org>
2020-03-26 02:20:42 +0300

    ci (.releaserc): adding file .releaserc

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,17 @@
 class Localizer {
-  private kek: string;
-  private ponki: string;
-  private lel: string;
-  private lol: string;
-  constructor(string: string) {
-    this.kek = string;
-    this.lel = "OGGG LOG";
-    this.lol = "lol";
-
-    this.ponki = this.kek + " WTF !?"
-  }
-  getKek(): string {
-    return this.kek;
-  }
-  setKek(string: string): string {
-    this.kek = string;
-
-    return this.kek;
-  }
-  getPonki(): string {
-    return this.ponki;
-  }
-  getLel(): string {
-    return this.lel;
-  }
-  getLol(): string {
-    return this.lol;
-  }
+    private kek: string;
+    constructor(string: string) {
+        this.kek = string;
+    }
+    getKek(): string {
+        return this.kek;
+    }
+    setKek(string: string): string {
+        this.kek = string;
+        return this.kek;
+    }
 }
 
 export {
-  Localizer
-}
-
+    Localizer
+};
